8e81ac17452d89dd6467f6d2548fa417eaf09704
Ricardo <ricardo.agnello@outlook.com>
2020-04-30 17:35:57 -0300

    Order, Instant, ISO 8601 part2

---
+++ src/main/java/com/neutronstar/course/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.neutronstar.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neutronstar.course.entities.Order;
+import com.neutronstar.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
