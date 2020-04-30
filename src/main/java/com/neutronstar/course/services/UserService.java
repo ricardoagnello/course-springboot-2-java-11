b981e6c6320a73472fe188f853e9e977490d362f
Ricardo <ricardo.agnello@outlook.com>
2020-04-30 16:55:08 -0300

    Service layer, component registration

---
+++ src/main/java/com/neutronstar/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.neutronstar.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neutronstar.course.entities.User;
+import com.neutronstar.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
