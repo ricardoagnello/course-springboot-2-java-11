2adbe1fa2cd0469be3c0d5aad1a269910afb3271
Ricardo <ricardo.agnello@outlook.com>
2020-04-30 15:09:45 -0300

    User entity and resource

---
+++ src/main/java/com/neutronstar/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.neutronstar.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neutronstar.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@email.com", "25418745", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
